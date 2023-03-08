fe7965a47092960b92d5cf90251af854562011d2
OtobeYoshiyuki <otobe1503@gmail.com>
2023-03-08 21:40:16 +0900

    当たり判定の検出用のクラス

---
+++ Assets/Script/Collision/HitChecker_Base.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OtobeLib
+{
+    /// <summary>
+    /// 当たり判定の有無を検知するクラス
+    /// ※抽象クラス
+    /// </summary>
+    public abstract class HitChecker_Base : MonoBehaviour
+    {
+        //当たり判定を取るべき相手オブジェクトを設定する
+        [SerializeField]
+        private List<string> m_objectsTag = new List<string>();
+
+        //オブジェクトに当たったかどうか検知するフラグ
+        //※衝突したらぶつかるものを検知する
+        protected bool m_isCollision = false;
+        public bool isCollision { get { return m_isCollision; } }
+
+        /// <summary>
+        /// オブジェクトのタグ一覧から当たり判定が設定されているものかどうか検索する
+        /// </summary>
+        /// <param name="objectTag">オブジェクトのタグ</param>
+        /// <returns>一覧にある場合はtrue、ない場合はfalse</returns>
+        protected bool Find_ObjectTag(string objectTag)
+        {
+            foreach (string tag in m_objectsTag)
+            {
+                //探していたものが見つかった場合はtrueを返す
+                if (tag == objectTag) return true;
+            }
+            //見つからなかった場合はfalseを返す
+            return false;
+        }
+    }
+}
---
+++ Assets/Script/Collision/HitChecker_Collision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OtobeLib
+{
+    /// <summary>
+    /// 当たり判定の衝突を検知するクラス
+    /// ※isTriggerがfalseのものにアタッチする
+    /// </summary>
+    public class HitChecker_Collision : HitChecker_Base
+    {
+        /// <summary>
+        /// UnityEvent オブジェクトに触れたときに呼ばれる
+        /// </summary>
+        /// <param name="collision">当たり判定の情報</param>
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            //当たり判定を設定しているものと触れた瞬間に、フラグをtrueにする
+            if (Find_ObjectTag(collision.gameObject.tag)) m_isCollision = true;
+        }
+
+        /// <summary>
+        /// UnityEvent オブジェクトから離れたときに呼ばれる
+        /// </summary>
+        /// <param name="collision">当たり判定の情報</param>
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            //当たり判定を設定しているものと離れた瞬間に、フラグをfalseにする
+            if (Find_ObjectTag(collision.gameObject.tag)) m_isCollision = false;
+        }
+    }
+}
